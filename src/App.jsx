69694c0bc4cccaf4e0766b40bc765e480ae40059
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-23 16:56:46 +0530

    feat: basic routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home, Login, Videos } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Videos />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { Home } from "./Home/Home";
+export { Login } from "./Login/Login";
+export { Videos } from "./Videos/Videos";
