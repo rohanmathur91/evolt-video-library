e62d18bca66ed5063030df2f8d7bd026a42f8697
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-26 19:11:56 +0530

    fix: update static data

--- src/staticData/sidebarData.js
+++ src/staticData/sidebarData.js
@@ -1,16 +1,16 @@
 export const sidebarData = [
   { path: "/home", icon: "home", optionName: "Home" },
   { path: "/explore", icon: "explore", optionName: "Exlore" },
-  { path: "/explore/playlist", icon: "video_library", optionName: "Playlist" },
+  { path: "/playlist", icon: "video_library", optionName: "Playlist" },
   {
-    path: "/explore/watch-later",
-    icon: "bookmark_border",
+    path: "/watch-later",
+    icon: "watch_later",
     optionName: "Watch later",
   },
   {
-    path: "/explore/liked-videos",
+    path: "/liked-videos",
     icon: "thumb_up",
     optionName: " Liked videos",
   },
-  { path: "/explore/history", icon: "history", optionName: "History" },
+  { path: "/history", icon: "history", optionName: "History" },
 ];
