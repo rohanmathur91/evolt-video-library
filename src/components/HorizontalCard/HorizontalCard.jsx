4261694bdfd0e59331539dee1f9c3979d6aa2302
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-26 18:38:09 +0530

    feat: add horizontal card component

---
+++ src/components/HorizontalCard/HorizontalCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styles from "./HorizontalCard.module.css";
+
+export const HorizontalCard = ({ video, handleRemoveFromPlaylist }) => {
+  const { _id, alt, thumbnail, views, duration, title } = video;
+
+  return (
+    <div className={`${styles.card} flex-row m-1 p-1 border rounded-sm`}>
+      <div className="w-20">
+        <img className="h-100 rounded-sm" src={thumbnail} alt={alt} />
+      </div>
+
+      <div className="flex-column py-1 px-2">
+        <h3 className={`${styles.title}`}>{title}</h3>
+
+        <div
+          className={`${styles.details} flex-row content-space-between pt-1`}
+        >
+          <div className="text-sm icon-container">
+            <span className="material-icons text-sm">visibility</span>
+            <span className={`${styles.views} text-sm`}>{views} views</span>
+          </div>
+
+          <div className="text-sm icon-container">
+            <span className="material-icons-outlined text-sm">timer</span>
+            <span className={`${styles.duration} text-sm`}>{duration}</span>
+          </div>
+        </div>
+      </div>
+
+      <button
+        onClick={() => handleRemoveFromPlaylist(_id)}
+        className={`${styles.remove__btn} icon-container p-1 rounded-sm`}
+      >
+        <span className="material-icons-outlined mx-1">delete</span>
+      </button>
+    </div>
+  );
+};
+
+HorizontalCard.defaultProps = {
+  video: {
+    _id: "",
+    alt: "thumbnail",
+    title: "",
+    thumbnail: "",
+    views: "0",
+    duration: "0:00",
+  },
+  handleRemoveFromPlaylist: () => {},
+};
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Sidebar } from "./Sidebar/Sidebar";
 export { VideoCard } from "./VideoCard/VideoCard";
 export { Explore } from "./Explore/Explore";
 export { Categories } from "./Categories/Categories";
+export { HorizontalCard } from "./HorizontalCard/HorizontalCard";
