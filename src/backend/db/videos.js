70af1cb1ba1e37f2fc09d427e8f0cdadfcd7c113
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-26 18:35:46 +0530

    fix: update videos data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,67 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "bInJjmH31Hk",
+    alt: "review-video",
+    views: "1.2M",
+    duration: "7:38",
+    title: "Sony WH-1000XM4 Review: The Final Form!",
+    thumbnail: "https://i.ytimg.com/vi/bInJjmH31Hk/hq720.jpg",
+    creatorName: "Marques Brownlee",
+    avatar:
+      "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
+    category: "review",
   },
+
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "Gvvo6vUpJRc",
+    alt: "review-video",
+    views: "7.2M",
+    duration: "15:24",
+    title: "AirPods Max Review: Luxury Listening!",
+    thumbnail: "https://i.ytimg.com/vi/Gvvo6vUpJRc/hq720.jpg",
+    creatorName: "Marques Brownlee",
+    avatar:
+      "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
+    category: "review",
+  },
+
+  {
+    _id: "ByayqNQU2fA",
+    alt: "review-video",
+    views: "1.9M",
+    duration: "9:02",
+    title: "Surface Headphones 2: Matte Black Everything!",
+    thumbnail: "https://i.ytimg.com/vi/ByayqNQU2fA/hq720.jpg",
+    creatorName: "Marques Brownlee",
+    avatar:
+      "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
+    category: "review",
+  },
+
+  {
+    _id: "YPln3JP_gKs",
+    alt: "review-video",
+    views: "3.2M",
+    duration: "6:49",
+    title: "Bose Headphones 700: The King is Back!",
+    thumbnail: "https://i.ytimg.com/vi/YPln3JP_gKs/hq720.jpg",
+    creatorName: "Marques Brownlee",
+    avatar:
+      "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
+    category: "review",
+  },
+
+  {
+    _id: "nuNYtj8keaE",
+    alt: "review-video",
+    views: "3.8M",
+    duration: "8:04",
+    title: "My Favorite Wireless Headphones | 2018!",
+    thumbnail: "https://i.ytimg.com/vi/nuNYtj8keaE/hq720.jpg",
+    creatorName: "Marques Brownlee",
+    avatar:
+      "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
+    category: "review",
   },
 ];
