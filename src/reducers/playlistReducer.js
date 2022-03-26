91711572ed8ca7f17d66922eb7682fad8371304c
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-26 22:00:55 +0530

    feat: update playlist-reducer for playlist management

--- src/reducers/playlistReducer.js
+++ src/reducers/playlistReducer.js
@@ -1,7 +1,17 @@
 export const playlistReducer = (playlistState, { type, payload }) => {
+  console.log(type, payload);
   switch (type) {
     case "SET_PLAYLIST":
       return { ...playlistState, playlists: payload };
+
+    case "ADD_TO_SAVED":
+      return { ...playlistState, saved: payload };
+
+    case "REMOVE_FROM_SAVED":
+      return {
+        ...playlistState,
+        watchLater: playlistState.saved.filter(({ _id }) => _id !== payload),
+      };
 
     case "ADD_TO_WATCH_LATER":
       return !playlistState.watchLater.some(({ _id }) => _id === payload._id)
@@ -18,6 +28,43 @@ export const playlistReducer = (playlistState, { type, payload }) => {
           ({ _id }) => _id !== payload
         ),
       };
+
+    case "ADD_TO_LIKED":
+      return { ...playlistState, liked: playlistState.liked.concat(payload) };
+
+    case "REMOVE_FROM_LIKED":
+      return {
+        ...playlistState,
+        watchLater: playlistState.liked.filter(({ _id }) => _id !== payload),
+      };
+
+    case "ADD_TO_PLAYLIST":
+      return {
+        ...playlistState,
+        playlists: playlistState.playlists.map((playlist) =>
+          playlist._id === payload.playlistId
+            ? { ...playlist, videos: playlist.videos.concat(payload.video) }
+            : playlist
+        ),
+      };
+
+    case "REMOVE_FROM_PLAYLIST":
+      return {
+        ...playlistState,
+        playlists: playlistState.playlists.map((playlist) =>
+          playlist._id === payload.playlistId
+            ? {
+                ...playlist,
+                videos: playlist.videos.filter(
+                  (video) => video._id !== payload.videoId
+                ),
+              }
+            : playlist
+        ),
+      };
+
+    case "UPDATE_CURRENT_PLAYLIST":
+      return { ...playlistState, currentPlaylist: payload };
 
     case "HANDLE_MODAL":
       return { ...playlistState, showModal: payload };
