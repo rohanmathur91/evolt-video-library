592d8a735c1d35796233b47a49856350970caf79
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-26 21:59:53 +0530

    fix: modal-reducer issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { Home } from "./Home/Home";
 export { Login } from "./Login/Login";
 export { VideoListing } from "./VideoListing/VideoListing";
 export { WatchLater } from "./WatchLater/WatchLater";
+export { Playlist } from "./Playlist/Playlist";
