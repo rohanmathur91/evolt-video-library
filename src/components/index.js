969471987c05910004b1538ea2154d16597df9f0
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-26 18:36:50 +0530

    feat: add categories component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar/Navbar";
 export { Sidebar } from "./Sidebar/Sidebar";
 export { VideoCard } from "./VideoCard/VideoCard";
 export { Explore } from "./Explore/Explore";
+export { Categories } from "./Categories/Categories";
