6e0c85c4ab26eb8b6b91de906dde9ef7daf15511
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-27 14:14:34 +0530

    feat: layout single video page

--- src/components/SingleVideo/SingleVideo.jsx
+++ src/components/SingleVideo/SingleVideo.jsx
@@ -1,6 +1,38 @@
 import React from "react";
 import styles from "./SingleVideo.module.css";
 
-export const SingleVideo = () => {
-  return <div>SingleVideo</div>;
+export const SingleVideo = ({ videoId }) => {
+  return (
+    <div className="main__container p-2">
+      <section className={`${styles.video}`}>
+        <iframe
+          src={`https://www.youtube.com/embed/rbCGmC1J1x8`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </section>
+      <section className={`${styles.details}`}>
+        <div>title</div>
+        <div className={`${styles.video__options}`}>
+          <button className={`${styles.btn}`}>
+            <span className="material-icons-outlined mr-2">thumb_up</span> Like
+          </button>
+
+          <button className={`${styles.btn}`}>
+            <span className="material-icons-outlined mr-2">turned_in_not</span>{" "}
+            Save to playlist
+          </button>
+
+          <button className={`${styles.btn}`}>
+            <span className="material-icons-outlined mr-2">watch_later</span>
+          </button>
+        </div>
+      </section>
+      <section className={`${styles.description}`}>description</section>
+    </div>
+  );
 };
+
+SingleVideo.defaultProps = { videoId: "" };
--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { SingleVideo } from "../../components";
 
 export const Video = () => {
-  return <SingleVideo />;
+  const { videoId } = useParams();
+
+  return <SingleVideo videoId={videoId} />;
 };
