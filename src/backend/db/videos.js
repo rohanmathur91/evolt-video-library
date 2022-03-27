309e581de194fde827838b3b4b1d6869eeb32fb0
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-27 16:57:46 +0530

    feat: add description for videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -15,6 +15,8 @@ export const videos = [
     avatar:
       "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
     category: "review",
+    description:
+      "Even better headphones. Still a terrible name. Digital noise cancelling: Industry leading Active Noise Cancellation (ANC) lends a personalized, virtually soundproof experience at any situation Voice assistant: Alexa, Google Assistant & Siri enabled (In-built) for voice access to music, information and more. Activate with a simple touch. Speak-to-chat: Headphones use an array of smart technologies to create a seamless, hands-free listening experience. For eg, simply start speaking to automatically pause your music in Speak-to-Chat. Wearing Detection: Proximity sensor and two acceleration sensors in your headphones can detect whether you are wearing them or not. then adapt playback accordingly to help save battery power",
   },
 
   {
@@ -28,6 +30,8 @@ export const videos = [
     avatar:
       "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
     category: "review",
+    description:
+      "Apple made some pretty great headphones with a pretty dumb case.While the Apple AirPod Max is a great pair of headphones with best-in-class active noise cancelling (ANC) and a good sound signature, it also has a few annoying quirks that make it very obviously an Apple product. If you live in the Apple ecosystem and already love your AirPods Pro, then the AirPods Max is more of the same just in a pair of over-ears.",
   },
 
   {
@@ -41,6 +45,8 @@ export const videos = [
     avatar:
       "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
     category: "review",
+    description:
+      "Microsoft. Do you think you can get me to like your headphones just for a new color? Surround yourself with spectacular Omnisonic sound for music, shows, and calls. Stay focused with adjustable noise cancellation. And, listen in comfort with up to 18.5 hours of music listening time.",
   },
 
   {
@@ -54,6 +60,8 @@ export const videos = [
     avatar:
       "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
     category: "review",
+    description:
+      "Terrible name. Excellent headphones. Companies compete; we win!. Critically acclaimed for their powerful noise cancelling, astonishing sound and unrivalled voice pickup, Bose Noise Cancelling Headphones 700 help turn any space into the perfect place to listen to music, get work done or just shut out the world for a few moments and relax. So if you’re looking for the best wireless Bluetooth® headphones for music and calls, you’ve found them.",
   },
 
   {
@@ -67,5 +75,7 @@ export const videos = [
     avatar:
       "https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s48-c-k-c0x00ffffff-no-rj",
     category: "review",
+    description:
+      "Better hop on this wireless train before it leaves the station...",
   },
 ];
