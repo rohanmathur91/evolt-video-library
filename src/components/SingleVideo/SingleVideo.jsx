a99b399c371e3408fd4147c5a4afae87a34c130e
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-27 16:45:01 +0530

    feat: add fetch each video by id

--- src/components/SingleVideo/SingleVideo.jsx
+++ src/components/SingleVideo/SingleVideo.jsx
@@ -1,37 +1,110 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import styles from "./SingleVideo.module.css";
 
-export const SingleVideo = ({ videoId }) => {
+export const SingleVideo = () => {
+  const { videoId } = useParams();
+  const [video, setVideo] = useState({});
+  const [loader, setLoader] = useState(false);
+  const { alt, views, duration, title, avatar, creatorName } = video;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoader(true);
+        const {
+          data: { video },
+        } = await axios.get(`/api/video/${videoId}`);
+
+        setVideo(video);
+        setLoader(false);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [videoId]);
+
   return (
-    <div className="p-2">
-      <section className={`${styles.container}`}>
-        <iframe
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </section>
-      <section className={`${styles.details}`}>
-        <div>title</div>
-        <div className={`${styles.video__options}`}>
-          <button className={`${styles.btn}`}>
-            <span className="material-icons-outlined mr-2">thumb_up</span> Like
-          </button>
+    <>
+      {loader ? (
+        <p className="text-sm text-center mb-2 p-2">Fetching video...</p>
+      ) : (
+        <div className="mb-2 p-2">
+          <section className={`${styles.container}`}>
+            <iframe
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </section>
+          <section>
+            <h4 className={`${styles.title} my-2`}>{title}</h4>
 
-          <button className={`${styles.btn}`}>
-            <span className="material-icons-outlined mr-2">turned_in_not</span>{" "}
-            Save to playlist
-          </button>
+            <div className="flex-row items-center mb-2 wrap">
+              <div className="flex-row">
+                <img
+                  alt={alt}
+                  src={avatar}
+                  className={`${styles.avatar} rounded-full mr-2`}
+                />
 
-          <button className={`${styles.btn}`}>
-            <span className="material-icons-outlined mr-2">watch_later</span>
-          </button>
+                <div className="flex-column content-space-between">
+                  <p className="font-semibold">{creatorName}</p>
+                  <div className="flex-row mt-auto">
+                    <div className="text-base icon-container mr-4">
+                      <span className="material-icons text-base mr-1">
+                        visibility
+                      </span>
+                      <span className={` text-sm`}>{views} views</span>
+                    </div>
+
+                    <div className="text-sm icon-container">
+                      <span className="material-icons-outlined text-base mr-1">
+                        timer
+                      </span>
+                      <span className={`text-sm`}>{duration}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={`${styles.video__options} flex-row my-2`}>
+                <button className="icon-container mr-3 font-semibold">
+                  <span className="material-icons-outlined mr-1">thumb_up</span>{" "}
+                  Like
+                </button>
+
+                <button className="icon-container mr-3 font-semibold">
+                  <span className="material-icons-outlined mr-1">
+                    turned_in_not
+                  </span>{" "}
+                  Save to playlist
+                </button>
+
+                <button className="icon-container font-semibold">
+                  <span className="material-icons-outlined mr-1">
+                    watch_later
+                  </span>{" "}
+                  Watch later
+                </button>
+              </div>
+            </div>
+          </section>
+          <section className={`${styles.description} text-sm `}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi
+            eveniet sequi in maiores voluptas odit laudantium rem, cum fugit
+            eaque qui facere ratione omnis. Tenetur, iure? Nemo pariatur
+            sapiente repellat? Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Quasi eveniet sequi in maiores voluptas odit
+            laudantium rem, cum fugit eaque qui facere ratione omnis. Tenetur,
+            iure? Nemo pariatur sapiente repellat?
+          </section>
         </div>
-      </section>
-      <section className={`${styles.description}`}>description</section>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import { Sidebar, SingleVideo } from "../../components";
 
 export const Video = () => {
-  const { videoId } = useParams();
-
   return (
     <div className="flex-row">
       <Sidebar />
-      <div className="main__container w-100 mt-1 px-2">
-        <SingleVideo videoId={videoId} />
+      <div className="main__container w-100 px-2">
+        <SingleVideo />
       </div>
     </div>
   );
