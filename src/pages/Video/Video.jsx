ef1a0d5ec27c746fa687b8357414a4d3ec497711
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-27 14:42:15 +0530

    fix: single video page sidebar

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { SingleVideo } from "../../components";
+import { Sidebar, SingleVideo } from "../../components";
 
 export const Video = () => {
   const { videoId } = useParams();
 
-  return <SingleVideo videoId={videoId} />;
+  return (
+    <div className="flex-row">
+      <Sidebar />
+      <div className="main__container w-100 mt-1 px-2">
+        <SingleVideo videoId={videoId} />
+      </div>
+    </div>
+  );
 };
