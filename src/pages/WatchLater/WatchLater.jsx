4976e1be218b2d70eb69f7a8a93be5ea47c0a166
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-28 09:58:10 +0530

    refactor: watch later page

--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { PlaylistContainer } from "../../components";
 import { usePlaylist } from "../../contexts";
+import { removeFromWatchLater } from "../../utils";
 
 export const WatchLater = () => {
   const { watchLater } = usePlaylist();
 
-  return <PlaylistContainer title="Watch later" videoList={watchLater} />;
+  return (
+    <PlaylistContainer
+      title="Watch later"
+      videoList={watchLater}
+      removeVideoHandler={removeFromWatchLater}
+    />
+  );
 };
