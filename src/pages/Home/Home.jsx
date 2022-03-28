a0b598d6a0ca05675251f288390eb849f84b30c2
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-29 02:29:22 +0530

    fix: background image of home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,25 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { useVideo } from "../../contexts";
+import { Footer, VideoOverviewCard } from "../../components";
 import styles from "./Home.module.css";
 
 export const Home = () => {
-  const navigate = useNavigate();
-  const { categories, videoDispatch } = useVideo();
-
-  const handleCategoryClick = (categoryName) => {
-    navigate("/explore");
-    videoDispatch({
-      type: "SET_CURRENT_CATEGORY",
-      payload: categoryName,
-    });
-  };
+  const { videos } = useVideo();
 
   return (
     <>
-      <header className={`${styles.header}`}>
-        <img src="https://i.ytimg.com/vi/bInJjmH31Hk/hq720.jpg" alt="banner" />
-      </header>
+      <header className={`${styles.header}`}></header>
       <section className="mb-4 px-3">
-        <h2 className="text-lg my-2">Categories</h2>
-        <div className={`${styles.categories}`}>
-          {categories.map(({ _id, categoryName }) => (
-            <p
-              key={_id}
-              onClick={() => handleCategoryClick(categoryName)}
-              className={`${styles.category} px-3 py-7 cursor-pointer font-semibold rounded-sm text-center`}
-            >
-              {categoryName}
-            </p>
-          ))}
+        <h2 className="text-lg mt-2">Videos</h2>
+        <div className={`${styles.videos} mt-1`}>
+          {videos?.length ? (
+            videos.slice(0, 4).map((video) => <VideoOverviewCard {...video} />)
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </section>
+      <Footer />
     </>
   );
 };
