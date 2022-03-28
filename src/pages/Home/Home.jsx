fbe47c9e17dd78fd8307e9cd293064ecbc2d91be
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-28 21:02:46 +0530

    fix(style): video card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useVideo } from "../../contexts";
 import styles from "./Home.module.css";
 
 export const Home = () => {
-  const { categories } = useVideo();
+  const navigate = useNavigate();
+  const { categories, videoDispatch } = useVideo();
+
+  const handleCategoryClick = (categoryName) => {
+    navigate("/explore");
+    videoDispatch({
+      type: "SET_CURRENT_CATEGORY",
+      payload: categoryName,
+    });
+  };
 
   return (
     <>
@@ -13,11 +23,13 @@ export const Home = () => {
       <section className="mb-4 px-3">
         <h2 className="text-lg my-2">Categories</h2>
         <div className={`${styles.categories} flex-row wrap`}>
-          {categories.map((category) => (
+          {categories.map(({ _id, categoryName }) => (
             <p
+              key={_id}
+              onClick={() => handleCategoryClick(categoryName)}
               className={`${styles.category} px-3 py-7 cursor-pointer font-semibold rounded-sm text-center`}
             >
-              {category.categoryName}
+              {categoryName}
             </p>
           ))}
         </div>
