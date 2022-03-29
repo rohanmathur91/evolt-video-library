eef2b0b3889ec0203be06d9b47e034e88f8579d3
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-29 16:35:58 +0530

    fix: update category backend data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,17 +9,21 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "New",
+    categoryURL: "https://i.ytimg.com/vi/PQBujSUwFk8/hq720.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Unboxing",
+    categoryURL: "https://i.ytimg.com/vi/-deneR4S8fI/hq720.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Comparison",
+    categoryURL: "https://i.ytimg.com/vi/cRNqVGfy2O4/hq720.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Noise Cancelling",
+    categoryURL: "https://i.ytimg.com/vi/cgR57T9Lnao/hq720.jpg",
   },
 ];
