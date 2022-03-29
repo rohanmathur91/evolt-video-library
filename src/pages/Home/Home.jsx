9f58c92f0feb2c5bc6bdcd6cd62bb550f6e6050b
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-29 14:13:29 +0530

    feat: add home page banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useVideo } from "../../contexts";
 import { Footer, VideoOverviewCard } from "../../components";
 import styles from "./Home.module.css";
@@ -8,7 +9,19 @@ export const Home = () => {
 
   return (
     <>
-      <header className={`${styles.header}`}></header>
+      <header className={`${styles.banner} flex-row flex-center`}>
+        <section className="mx-2 flex-column items-center">
+          <h1 className={`${styles.heading} mb-4`}>
+            View quality reviews for your headphones.
+          </h1>
+          <Link
+            to="/explore"
+            className={`${styles.cta} py-2 px-4 text-sm transition-2 rounded-sm font-bold mt-1`}
+          >
+            Explore
+          </Link>
+        </section>
+      </header>
       <section className="mb-4 px-3">
         <h2 className="text-lg mt-2">Videos</h2>
         <div className={`${styles.videos} mt-1`}>
