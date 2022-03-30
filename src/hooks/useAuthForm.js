ae0b6251a06e5c1fcf585a50e49f7aa9ec67167c
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-30 15:32:47 +0530

    refactor: exports from useAuthform hook

--- src/hooks/useAuthForm.js
+++ src/hooks/useAuthForm.js
@@ -2,14 +2,11 @@ import { useReducer } from "react";
 import { authFormReducer } from "../reducers";
 
 export const useAuthForm = (initialForm) => {
-  const [{ loading, showPassword, credentials }, authFormDispatch] = useReducer(
-    authFormReducer,
-    {
-      loading: false,
-      showPassword: false,
-      credentials: initialForm,
-    }
-  );
+  const [authFormState, authFormDispatch] = useReducer(authFormReducer, {
+    loading: false,
+    showPassword: false,
+    credentials: initialForm,
+  });
 
-  return { loading, showPassword, credentials, authFormDispatch };
+  return { ...authFormState, authFormDispatch };
 };
